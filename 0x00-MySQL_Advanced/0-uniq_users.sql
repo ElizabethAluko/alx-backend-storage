35db56916577d637e92b91ca39e001f76417ded7
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-11 15:48:15 +0100

    Add task0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- Creates a table users with:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null
+-- unique name, string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) UNIQUE,
+	name VARCHAR(255)
+)
