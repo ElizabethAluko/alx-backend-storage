f0f1ba7bc5cc289beef7cf5b61cbab977280705b
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-13 03:58:51 +0100

    Add task8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,12 @@
 -- creates an index idx_name_first on the table names and the first letter of name.
 
+-- Add a new column to store the first letter of the name
+ALTER TABLE names
+ADD COLUMN first_letter CHAR(1);
 
-CREATE INDEX idx_name_first ON names ((LEFT(name, 1)));
+-- Update the new column with the first letter of the name
+UPDATE names
+SET first_letter = LEFT(name, 1);
+
+-- Create an index on the first letter column
+CREATE INDEX idx_name_first ON names (first_letter);
