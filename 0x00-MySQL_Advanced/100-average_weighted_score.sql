79a6157a0568b15332875417a0661ce022468f68
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-13 03:29:11 +0100

    Add task100

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -9,13 +9,14 @@ BEGIN
     DECLARE avg_weighted_score DECIMAL(10, 2);
 
     -- Calculate the average weighted score for the user
-    SELECT SUM(score * weight) / SUM(weight) INTO avg_weighted_score
-    FROM corrections
-    WHERE user_id = user_id;
+    SELECT SUM(c.score * p.weight) / SUM(p.weight) INTO avg_weighted_score
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
 
     -- Update the user's average weighted score in the users table
     UPDATE users
-    SET average_weighted_score = avg_weighted_score
+    SET average_score = avg_weighted_score
     WHERE id = user_id;
 END //
 
