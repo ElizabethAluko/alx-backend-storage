f73ade8ab7107037f2a06f3034f6bb184bbadf74
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-17 00:41:58 +0100

    Add task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,18 +1,53 @@
 #!/usr/bin/env python3
 """
-This module defines the Cache class, which allows storing and retrieving data in a Redis cache.
+This module defines the Cache class, related decorators, replay function, and main code.
 """
 
 import uuid
 import redis
 from typing import Union, Callable
+from functools import wraps
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts the number of calls to a method.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator that stores the history of inputs and outputs for a function.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+        self._redis.rpush(input_key, str(args))
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+        return result
+    return wrapper
 
 class Cache:
     """
     A class for storing and retrieving data in a Redis cache.
-
-    Attributes:
-        _redis (redis.Redis): The Redis client instance.
     """
 
     def __init__(self):
@@ -22,6 +57,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores the provided data in the Redis cache.
@@ -75,3 +112,25 @@ class Cache:
             Union[int, None]: The retrieved integer, or None if key does not exist.
         """
         return self.get(key, fn=int)
+
+
+def replay(method: Callable) -> None:
+    """
+    Displays the history of calls for a given method.
+
+    Args:
+        method (Callable): The method to display history for.
+    """
+    input_key = method.__qualname__ + ":inputs"
+    output_key = method.__qualname__ + ":outputs"
+    inputs = [eval(args_str) for args_str in cache._redis.lrange(input_key, 0, -1)]
+    outputs = cache._redis.lrange(output_key, 0, -1)
+    print(f"{method.__qualname__} was called {len(inputs)} times:")
+    for args, output in zip(inputs, outputs):
+        print(f"{method.__qualname__}{args} -> {output.decode('utf-8')}")
+
+cache = Cache()
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
+replay(cache.store)
