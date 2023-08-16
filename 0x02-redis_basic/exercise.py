639617bd9c832db89435416b9827d8f4e8a3ca09
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-16 21:00:42 +0100

    Add task1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
-This module defines the Cache class, which allows storing data in a Redis cache.
+This module defines the Cache class, which allows storing and retrieving data in a Redis cache.
 """
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     """
-    A class for storing data in a Redis cache.
+    A class for storing and retrieving data in a Redis cache.
 
     Attributes:
         _redis (redis.Redis): The Redis client instance.
@@ -35,3 +35,43 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        Retrieves data from the Redis cache using the provided key.
+
+        Args:
+            key (str): The key under which the data is stored in Redis.
+            fn (Callable, optional): A callable to convert the data to the desired format.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data, or None if key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieves a string from the Redis cache using the provided key.
+
+        Args:
+            key (str): The key under which the data is stored in Redis.
+
+        Returns:
+            Union[str, None]: The retrieved string, or None if key does not exist.
+        """
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieves an integer from the Redis cache using the provided key.
+
+        Args:
+            key (str): The key under which the data is stored in Redis.
+
+        Returns:
+            Union[int, None]: The retrieved integer, or None if key does not exist.
+        """
+        return self.get(key, fn=int)
