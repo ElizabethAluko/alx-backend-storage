b5a2de37984b5fce6146ed2ec712ce3fc8d0b958
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-17 01:16:55 +0100

    Add task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,48 +6,38 @@ This module defines the get_page function that fetches and caches web pages usin
 import redis
 import requests
 
-class WebCache:
+def get_page(url: str) -> str:
     """
-    A class for caching web pages using Redis.
+    Fetches and caches the HTML content of a web page using Redis.
+
+    Args:
+        url (str): The URL of the web page.
+
+    Returns:
+        str: The HTML content of the web page.
     """
+    redis_instance = redis.Redis()
 
-    def __init__(self):
-        """
-        Initializes the WebCache instance and connects to Redis.
-        """
-        self._redis = redis.Redis()
+    count_key = f"count:{url}"
+    content_key = f"content:{url}"
 
-    def get_page(self, url: str) -> str:
-        """
-        Fetches and caches the HTML content of a web page using Redis.
+    # Increment the access count for this URL
+    redis_instance.incr(count_key)
 
-        Args:
-            url (str): The URL of the web page.
+    # Check if the content is already cached
+    cached_content = redis_instance.get(content_key)
+    if cached_content:
+        return cached_content.decode("utf-8")
 
-        Returns:
-            str: The HTML content of the web page.
-        """
-        count_key = f"count:{url}"
-        content_key = f"content:{url}"
+    # Fetch the page content using requests
+    response = requests.get(url)
+    content = response.text
 
-        # Increment the access count for this URL
-        self._redis.incr(count_key)
+    # Cache the content with a 10-second expiration
+    redis_instance.setex(content_key, 10, content)
 
-        # Check if the content is already cached
-        cached_content = self._redis.get(content_key)
-        if cached_content:
-            return cached_content.decode("utf-8")
+    return content
 
-        # Fetch the page content using requests
-        response = requests.get(url)
-        content = response.text
-
-        # Cache the content with a 10-second expiration
-        self._redis.setex(content_key, 10, content)
-
-        return content
-
-web_cache = WebCache()
 url = "http://slowwly.robertomurray.co.uk/delay/1000/url/https://www.example.com"
-page_content = web_cache.get_page(url)
+page_content = get_page(url)
 print(page_content)
